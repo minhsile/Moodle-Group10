8c54f3544b309617b0f269c9952b26366d3255bf
minhsile <hungtiensi.lms@gmail.com>
2022-01-09 10:40:24 +0700

    Init

---
+++ server/moodledata/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = 'nTC6fKyMggwGWLBQ4kfy9fDz2EPN24uYlocalhost';
+$CFG->bootstraphash = '1290a75c964a7371d708149451c07432';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
